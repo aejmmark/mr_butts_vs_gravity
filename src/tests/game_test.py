9ef45bf32fa95ddf231f9c307d4a0406e8dc658d
aejmmark <aejmelaeus.markus@gmail.com>
2021-04-14 20:57:22 +0300

    bug fix and more tests

--- src/tests/game_test.py
+++ src/tests/game_test.py
@@ -2,24 +2,25 @@
 import unittest
 import game
 import pygame
+from constants import TEST_LEVEL, TEST_FLAG, NO_FLAG
 
 class TestGame(unittest.TestCase):
     def setUp(self):
         self.game = game.Game()
-        self.game.start()
-
+        self.game.start(TEST_LEVEL, TEST_FLAG)
+        self.game.player.pos.x = 400
+        self.game.player.pos.y = 370
+                
     def test_jump(self):
-        self.pos = self.game.player.pos.y
         self.game.new_event(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_SPACE))
-        self.game.run(10)
-        self.new_pos = self.game.player.pos.y
+        self.game.run(1)
         self.game.quit_game()
-        self.assertTrue(self.new_pos < self.pos)
-    
+        self.assertTrue(self.game.player.vel.y < -7)
+
     def test_left_movement(self):
         self.pos = self.game.player.pos.x
         self.game.new_event(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_LEFT))
-        self.game.run(10)
+        self.game.run(1)
         self.new_pos = self.game.player.pos.x
         self.game.quit_game()
         self.assertTrue(self.new_pos < self.pos)
@@ -27,20 +28,26 @@ class TestGame(unittest.TestCase):
     def test_right_movement(self):
         self.pos = self.game.player.pos.x
         self.game.new_event(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_RIGHT))
-        self.game.run(10)
+        self.game.run(1)
         self.new_pos = self.game.player.pos.x
         self.game.quit_game()
         self.assertTrue(self.new_pos > self.pos)
 
     def test_win_condition(self):
+        self.game.player.pos.x = 231
+        self.game.player.pos.y = 370
         self.pos = self.game.player.pos.x
         self.game.new_event(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_LEFT))
-        self.game.run(200)
-        self.new_pos = self.game.player.pos.x
+        self.game.run(5)
         self.game.quit_game()
-        self.assertTrue(self.new_pos < self.pos)
+        self.assertTrue(self.game.win)
+
+    def test_x_button_quits(self):
+        self.game.new_event(pygame.event.Event(pygame.QUIT))
+        self.game.run(1)
+        self.assertFalse(self.game.running)
 
     def test_quitting_wont_win(self):
-        self.game.run(10)
+        self.game.run(1)
         self.game.quit_game()
         self.assertFalse(self.game.win)
