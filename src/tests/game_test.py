4c9697cc8c2672d8fe45137919f270967b3f792a
aejmmark <aejmelaeus.markus@gmail.com>
2021-04-14 17:35:59 +0300

    tests for winning and quitting

--- src/tests/game_test.py
+++ src/tests/game_test.py
@@ -11,7 +11,7 @@ class TestGame(unittest.TestCase):
     def test_jump(self):
         self.pos = self.game.player.pos.y
         self.game.new_event(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_SPACE))
-        self.game.run(20)
+        self.game.run(10)
         self.new_pos = self.game.player.pos.y
         self.game.quit_game()
         self.assertTrue(self.new_pos < self.pos)
@@ -19,7 +19,7 @@ class TestGame(unittest.TestCase):
     def test_left_movement(self):
         self.pos = self.game.player.pos.x
         self.game.new_event(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_LEFT))
-        self.game.run(20)
+        self.game.run(10)
         self.new_pos = self.game.player.pos.x
         self.game.quit_game()
         self.assertTrue(self.new_pos < self.pos)
@@ -27,7 +27,20 @@ class TestGame(unittest.TestCase):
     def test_right_movement(self):
         self.pos = self.game.player.pos.x
         self.game.new_event(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_RIGHT))
-        self.game.run(20)
+        self.game.run(10)
         self.new_pos = self.game.player.pos.x
         self.game.quit_game()
         self.assertTrue(self.new_pos > self.pos)
+
+    def test_win_condition(self):
+        self.pos = self.game.player.pos.x
+        self.game.new_event(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_LEFT))
+        self.game.run(200)
+        self.new_pos = self.game.player.pos.x
+        self.game.quit_game()
+        self.assertTrue(self.new_pos < self.pos)
+
+    def test_quitting_wont_win(self):
+        self.game.run(10)
+        self.game.quit_game()
+        self.assertFalse(self.game.win)
