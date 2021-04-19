4b2a79394ca86f548bae35df60104da3e3875c68
aejmmark <aejmelaeus.markus@gmail.com>
2021-04-19 11:15:16 +0300

    Bug fix

--- src/stage.py
+++ src/stage.py
@@ -22,39 +22,42 @@ class Stage:
 
     def scroll(self):
         """Moves the screen diagonally"""
-        self.player.pos.x -= 0.5
+        self.player.pos.x -= 1
         for platform in self.platforms:
-            platform.pos.x -= 0.5
+            platform.pos.x -= 1
         for baddie in self.baddies:
-            baddie.pos.x -= 3
+            baddie.pos.x -= 2.5
             if self.player.pos.y < baddie.pos.y:
-                baddie.pos.y -= 0.1
+                baddie.pos.y -= 0.2
             if self.player.pos.y > baddie.pos.y:
-                baddie.pos.y += 0.1
+                baddie.pos.y += 0.2
+        self.timer += 1
+        if self.timer == 1800:
+            self.difficulty += 1
+            self.timer = 0
 
     def generate(self):
         """Generates new platforms"""
         while len(self.platforms) < 8:
             pos_x = randint(WIDTH + 50,WIDTH + 200)
-            pos_y = randint(100, 400)
+            pos_y = randint(50, 450)
             width = randint(80, 120)
             height = randint(20, 45)
             new_plat = Platform(pos_x, pos_y, width, height)
-            for platform in self.platforms:
-                if (abs(new_plat.pos.x - platform.pos.x) < 150) \
-                    and (abs(new_plat.pos.y - platform.pos.y) < 100):
-                    new_plat.kill()
-                    break
-                else:
-                    self.platforms.add(new_plat)
-                    self.all_sprites.add(new_plat)
-                    self.timer += 1
-                    if self.timer == 100:
-                        self.difficulty += 1
-                        self.timer = 0
+            self.check_overlap(new_plat, self.platforms, 150, 100)
         while len(self.baddies) < self.difficulty:
             pos_x = randint(WIDTH + 50,WIDTH + 200)
             pos_y = randint(-30,HEIGHT + 30)
-            baddie = Baddie(pos_x, pos_y)
-            self.baddies.add(baddie)
-            self.all_sprites.add(baddie)
+            new_baddie = Baddie(pos_x, pos_y)
+            self.check_overlap(new_baddie, self.baddies, 50, 50)
+
+    def check_overlap(self, new_sprite, group, x_pos, y_pos):
+        """Checks if the given sprite overlaps with any in the given group.
+        Uses the given measurements"""
+        for sprite in group:
+            if (abs(new_sprite.pos.x - sprite.pos.x) < x_pos) \
+                and (abs(new_sprite.pos.y - sprite.pos.y) < y_pos):
+                new_sprite.kill()
+                return
+        group.add(new_sprite)
+        self.all_sprites.add(new_sprite)
